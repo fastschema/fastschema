90bd640142318598f543ba63419986eb63210008
nnson2 <ym1234567891@gmail.com>
2024-05-23 13:04:33 +0700

    applied for github, gooogle provider

---
+++ services/auth/auth.go
@@ -0,0 +1,166 @@
+package authservice
+
+import (
+	// "errors"
+	"fmt"
+
+	"github.com/fastschema/fastschema/db"
+	"github.com/fastschema/fastschema/fs"
+	"github.com/fastschema/fastschema/pkg/errors"
+	"github.com/fastschema/fastschema/pkg/utils"
+	"github.com/fastschema/fastschema/schema"
+	userservice "github.com/fastschema/fastschema/services/user"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/github"
+	"golang.org/x/oauth2/google"
+)
+
+type AppLike interface {
+	DB() db.Client
+	Key() string
+}
+
+type AuthProvider struct {
+	config *oauth2.Config
+}
+
+const (
+	Github = "github"
+	Google = "google"
+)
+
+type ProviderUsers struct {
+	Github GithubUserResponse `json:"github"`
+	Google GoogleUserResponse `json:"google"`
+}
+
+type AuthService struct {
+	DB          func() db.Client
+	AppKey      func() string
+	OAuthGithub AuthProvider
+	OAuthGoogle AuthProvider
+}
+
+func New(app AppLike) *AuthService {
+	authService := &AuthService{
+		DB:     app.DB,
+		AppKey: app.Key,
+	}
+
+	if utils.Env("GITHUB_PROVIDER_ENABLED") == "true" {
+		authService.OAuthGithub = AuthProvider{
+			config: &oauth2.Config{
+				ClientID:     utils.Env("GITHUB_CLIENT_ID"),
+				ClientSecret: utils.Env("GITHUB_CLIENT_SECRET"),
+				RedirectURL:  "http://localhost:8000/api/auth/github/callback",
+				Endpoint:     github.Endpoint,
+			},
+		}
+	}
+
+	if utils.Env("GOOGLE_PROVIDER_ENABLED") == "true" {
+		authService.OAuthGoogle = AuthProvider{
+			config: &oauth2.Config{
+				ClientID:     utils.Env("GOOGLE_CLIENT_ID"),
+				ClientSecret: utils.Env("GOOGLE_CLIENT_SECRET"),
+				Endpoint:     google.Endpoint,
+				RedirectURL:  "http://localhost:8000/api/auth/google/callback",
+				Scopes: []string{
+					"https://www.googleapis.com/auth/userinfo.email",
+					"https://www.googleapis.com/auth/userinfo.profile",
+				},
+			},
+		}
+	}
+
+	return authService
+}
+
+func (as *AuthService) Login(c fs.Context, _ any) (u *schema.Schema, err error) {
+
+	if c.Arg("provider") == Github {
+		return as.LoginGithub(c, nil)
+	}
+	if c.Arg("provider") == Google {
+		return as.LoginGoogle(c, nil)
+	}
+	return nil, errors.New("invalid provider")
+
+}
+
+func (as *AuthService) Callback(c fs.Context, _ any) (u *userservice.LoginResponse, err error) {
+	if c.Arg("provider") == Github {
+		return as.CallbackGithub(c, nil)
+	}
+	if c.Arg("provider") == Google {
+		return as.CallbackGoogle(c, nil)
+	}
+	return nil, errors.New("invalid provider")
+}
+
+func (as *AuthService) processLoginResponse(c fs.Context, providerUsers ProviderUsers, provider string) (*userservice.LoginResponse, error) {
+	var query *db.Predicate
+	if provider == Github {
+		query = db.EQ("username", providerUsers.Github.Login)
+	} else if provider == Google {
+		query = db.EQ("email", providerUsers.Google.Email)
+	}
+
+	userExisted, _ := db.Query[*fs.User](as.DB()).Where(query).First(c.Context())
+
+	if userExisted != nil {
+		if !userExisted.Active {
+			return nil, errors.Unauthorized("user is not active")
+		}
+		jwtToken, exp, err := userExisted.JwtClaim(as.AppKey())
+		if err != nil {
+			return nil, err
+		}
+		fmt.Println("jwtToken ", jwtToken, exp)
+
+		return &userservice.LoginResponse{Token: jwtToken, Expires: exp}, nil
+	}
+
+	userRole, err := db.Query[*fs.Role](as.DB()).Where(db.EQ("name", "User")).First(c.Context())
+	if err != nil {
+		e := utils.If(db.IsNotFound(err), errors.NotFound, errors.InternalServerError)
+		return nil, e(err.Error())
+	}
+
+	var userSaved *fs.User
+	if provider == Github {
+		userSaved, err = db.Create[*fs.User](c.Context(), as.DB(), schema.NewEntityFromMap(map[string]any{
+			"username": providerUsers.Github.Login,
+			"email":    providerUsers.Github.Email,
+			"active":   true,
+			"provider": Github,
+			// "provider_id":       providerUsers.Github.ID,
+			"provider_username": providerUsers.Github.Login,
+			"roles": []*schema.Entity{
+				schema.NewEntity(userRole.ID),
+			},
+		}))
+	} else if provider == Google {
+		userSaved, err = db.Create[*fs.User](c.Context(), as.DB(), schema.NewEntityFromMap(map[string]any{
+			"username":    "", // need to fix to create user without username
+			"email":       providerUsers.Google.Email,
+			"active":      true,
+			"provider":    Google,
+			"provider_id": providerUsers.Google.ID,
+			// "provider_username": providerUsers.Google.Name,
+			"roles": []*schema.Entity{
+				schema.NewEntity(userRole.ID),
+			},
+		}))
+	}
+
+	if err != nil {
+		return nil, err
+	}
+	jwtToken, exp, err := userSaved.JwtClaim(as.AppKey())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("userSaved %v ", userSaved)
+	return &userservice.LoginResponse{Token: jwtToken, Expires: exp}, nil
+}
---
+++ services/auth/google.go
@@ -0,0 +1,75 @@
+package authservice
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/fastschema/fastschema/fs"
+	"github.com/fastschema/fastschema/schema"
+	userservice "github.com/fastschema/fastschema/services/user"
+)
+
+const oauthGoogleUrlAPI = "https://www.googleapis.com/oauth2/v2/userinfo?access_token="
+
+type GoogleUserResponse struct {
+	ID      string `json:"id"`
+	Email   string `json:"email"`
+	Name    string `json:"name"`
+	Picture string `json:"picture"`
+}
+
+func (as *AuthService) GetGoogleUserFromAccessCode(code string) (*GoogleUserResponse, error) {
+	token, err := as.OAuthGoogle.config.Exchange(context.Background(), code)
+	if err != nil {
+		return nil, fmt.Errorf("code exchange wrong: %s", err.Error())
+	}
+	response, err := http.Get(oauthGoogleUrlAPI + token.AccessToken)
+	if err != nil {
+		return nil, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed read response: %s", err.Error())
+	}
+
+	userResponse := &GoogleUserResponse{}
+	if err := json.Unmarshal(body, userResponse); err != nil {
+		return nil, err
+	}
+
+	return userResponse, nil
+}
+
+func (as *AuthService) LoginGoogle(c fs.Context, _ any) (u *schema.Schema, err error) {
+	url := as.OAuthGoogle.config.AuthCodeURL("randomstate")
+	fmt.Println("url", url)
+	c.Redirect(url)
+	return nil, nil
+}
+
+func (as *AuthService) CallbackGoogle(c fs.Context, _ any) (u *userservice.LoginResponse, err error) {
+	if c.Arg("state") != "randomstate" {
+		return nil, fmt.Errorf("invalid oauth google state")
+	}
+
+	if c.Arg("code") == "" {
+		return nil, fmt.Errorf("code is empty")
+	}
+
+	googleUser, err := as.GetGoogleUserFromAccessCode(c.Arg("code"))
+
+	fmt.Println("googleUser", googleUser)
+
+	if err != nil {
+		return nil, err
+	}
+	providerUsers := ProviderUsers{
+		Google: *googleUser,
+	}
+	return as.processLoginResponse(c, providerUsers, Google)
+}
